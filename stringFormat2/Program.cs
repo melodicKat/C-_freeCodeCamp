d0a4e4699f2c6ec169117d36eeca636290678449
melodicKat <favcolbun2@gmail.com>
2024-07-26 22:26:36 +0700

    update string format 2

---
+++ stringFormat2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace stringfomated
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine(@"conmeo/conmhco");
+            Console.WriteLine($" ");  
+            Console.WriteLine(@""); 
+            Console.WriteLine(@"conmeo/conmhco");
+
+            Console.ReadLine();
+        }
+    }
+}
