e87919fbef338704337bc15c3fe80e7407c435d9
melodicKat <favcolbun2@gmail.com>
2024-08-17 00:07:30 +0700

    event-delegate

--- test_Knowledge_After_Break/Program.cs
+++ test_Knowledge_After_Break/Program.cs
@@ -1,172 +1,68 @@
 ﻿using System;
-using System.CodeDom;
-using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices.ComTypes;
 
-namespace baitpapTest
+namespace conmeo 
 {
-
-    interface hoTro
-    {
-         decimal soTienHoTro();
-    }
-    abstract internal class HopDongCaSi
-    {
-        protected string maHopDong;
-        protected string tenCaSi;
-        protected int soTietMucBieuDien;
-        protected string ngayBieuDien;
-        protected decimal donGia;
-        protected decimal phuPhi;
-        public string MaHopDong
-        {
-            set
-            {
-                maHopDong = value;
-                bool checkTrue = true;
-                foreach (char temp in value.Substring(2, 3))
-                {
-                    if (!char.IsDigit(temp))
-                        checkTrue = false;
-                }
-
-                if (!(value.Length == 6) || !(value.StartsWith("HD") && checkTrue))
-                {
-                    throw new Exception("Loi");
-                }
-            }
-                  
-            get => maHopDong; 
-        }
-        public string TenCaSi { set => tenCaSi = value; get => tenCaSi; }
-        public int SoTietMucBieuDien { get => soTietMucBieuDien; set => soTietMucBieuDien = value; }
-        public string NgayBieuDien { get => ngayBieuDien; set => ngayBieuDien = value; }
-        public decimal DonGia { get => donGia; set => donGia = value; }
-        public decimal PhuPhi { get => phuPhi; set => phuPhi = value; }
-
-        //constructor
-
-        // 3
-        
-        public HopDongCaSi()
-        {
-            this.maHopDong = "HD3418";
-            this.tenCaSi = "Ly Khong Hay";
-            this.soTietMucBieuDien = 3;
-            this.ngayBieuDien = "24/10/2020";
-            this.donGia = 4000000M;
-            this.phuPhi = 2000000M;
-        }
-
-        //4
-
-        public HopDongCaSi(string maHopDong, string tenCaSi, decimal donGia)
-
-        {
-            this.maHopDong = maHopDong;
-            this.tenCaSi = tenCaSi;
-            soTietMucBieuDien = 2;
-            ngayBieuDien = "24/12/2020";
-            this.donGia = donGia;
-            phuPhi = 0M;
-        }
-        abstract public decimal quangCao();
-        virtual public decimal thanhTien()
-        {
-            return donGia * soTietMucBieuDien + phuPhi + quangCao();
-        }
-
-        virtual public decimal thue()
-        {
-            return 0.15M * thanhTien();
-        }
-
-    }
-
-
-    #region hopDongBieuDien_taiNHa
-    /// <summary>
-    /// inherited by abstract class hopDongCaSi and hoTro interface
-    /// </summary>
-    internal class BieuDienTaiNha : HopDongCaSi
-    {
-        private decimal khoangCach;
-        public decimal KhoangCach { get => khoangCach; set => khoangCach = value; }
-        override public decimal quangCao()
-        {
-            if (khoangCach < 20)
-                return  (decimal)(400000M * khoangCach);
-            else if ((khoangCach * 300000) < 15000000)
-                return (300000M* khoangCach);
-            return 15000000M;
-
-        }
-    }
-    #endregion
-
-    #region hopDongBieuDien_taiRapNho
-    internal class BieuDienTaiRapNho : HopDongCaSi, hoTro
-    {
-        private int quiMo;
-
-        public int QuiMo
-        {
-            get => quiMo;
-            set => quiMo = value;
-        }
-        override public decimal quangCao()
-        {
-            return 10000000M + 2000000M * soTietMucBieuDien; 
-        }
-
-        public decimal soTienHoTro()
-        {
-            if( 1 <= quiMo || quiMo <= 3)
-            {
-                return 1000000;
-            }
-            return 0;
-        }
-        public override decimal thanhTien()
-        {
-            return base.thanhTien() - soTienHoTro();
-        }
-    }
-    #endregion
-    internal class BieuDienTaiRapLon: HopDongCaSi, hoTro
-    {
-        private decimal khanGia;
-
-        public decimal KhanGia { get => khanGia; set => khanGia = value; }
-
-        override public decimal quangCao()
-        {
-            if (KhanGia < 1000000)
-                return 30000000M;
-            return 50000000M;
-        }
-
-        public decimal soTienHoTro()
-        {
-            if (KhanGia < 2000000)
-                return ((int)KhanGia % 100000) * 1000000M;
-            return 20000000;
-        }
-        public override decimal thanhTien()
-        {
-            return base.thanhTien() - soTienHoTro();
-        }
-
-    }
-
-
     internal class Program
     {
+        delegate void updateNameHandler(string name);
         static void Main(string[] args)
         {
-            HopDongCaSi a = new BieuDienTaiNha();
-            
+            hocSinh a = new hocSinh();
+            a.eventNameChange += ShowChange;
+            a.Name = "conmeo";
+            a.Name = "conmeo";
+            a.Name = "conmdf";
+
+            hocSinh b = new hocSinh();
+            b.NameChange = ShowChange;
+            b.Name = "conmeo";
+            b.Name = "condsafasdo";
+            b.Name = "conmdf";
+
+            hocSinh c = new hocSinh();
+            c.NameChange = ShowChange;
+            c.Name = "conmeo";
+            c.Name = "conmeo";
+            c.Name = "conmdf";
 
             Console.ReadKey();
+
+
         }
+
+        static void ShowChange(string name)
+        {
+            Console.WriteLine("Name changed to: " + name);
+        }
+
+        class hocSinh
+        {
+
+
+            public updateNameHandler namechange;
+
+            public updateNameHandler NameChange { set => namechange = value; }
+
+            public event updateNameHandler eventNameChange;
+           
+            private string name;
+            public string Name
+            {
+                get => name;
+                set 
+                { 
+                    name = value;
+                 
+                    eventNameChange?.Invoke(value);
+                
+                }
+            }
+        }
+        
     }
+
 }
--- test_Knowledge_After_Break/testPart.cs
+++ test_Knowledge_After_Break/testPart.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace sinhVienCls
+{
+    internal class sinhvien : IComparer
+    {
+        public int age { get; set; }
+        public decimal avr { get; set; }
+        public string name { get; set; }
+
+        public sinhvien()
+        {
+
+        }
+        public sinhvien(int age, decimal avr, string name)
+        {
+            this.age = age;
+            this.avr = avr;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"std name : {this.name} / avr : {this.avr} / age : {this.age}";
+        }
+
+        public int Compare(object a, object b)
+        {
+            sinhvien x = a as sinhvien;
+            sinhvien y = b as sinhvien;
+
+            return x.age.CompareTo(y.age);
+        }
+
+    }
+    internal class personCompare : IComparer
+    {
+        public int Compare(object a, object b)
+        {
+            sinhvien x = a as sinhvien;
+            sinhvien y = b as sinhvien;
+
+            return decimal.Compare(x.avr, y.avr);
+        }
+
+    }
+}
